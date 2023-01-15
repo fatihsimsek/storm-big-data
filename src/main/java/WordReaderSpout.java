8046321568df468fb4da3716ef0e2dfc2cabf154
Fatih <fatih.simsek@akbank.com>
2023-01-15 18:19:08 +0300

    update

--- src/main/java/WordReaderSpout.java
+++ src/main/java/WordReaderSpout.java
@@ -5,6 +5,12 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class WordReaderSpout implements IRichSpout, CompletableSpout {
@@ -12,22 +18,14 @@ public class WordReaderSpout implements IRichSpout, CompletableSpout {
     private static final long serialVersionUID = 441966625018520917L;
     private SpoutOutputCollector collector;
 
-    private String[] sentences = {
-            "Hello World",
-            "Apache Storm",
-            "Big Data",
-            "Test Big Data",
-            "Machine Learning",
-            "Hello World",
-            "Test",
-            "Data"
-    };
+    private List<String> sentences = new ArrayList<>();
 
     boolean isCompleted;
     String fileName;
 
     public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
         this.collector = collector;
+        this.sentences = readInput("input.txt");
     }
 
     public void close() {
@@ -70,5 +68,18 @@ public class WordReaderSpout implements IRichSpout, CompletableSpout {
     @Override
     public boolean isExhausted() {
         return this.isCompleted;
+    }
+
+    private List<String> readInput(String path) {
+        List<String> result = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
